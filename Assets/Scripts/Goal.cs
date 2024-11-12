aedb51bd7c693b6e12a351aacd3e998e749539dc
Ishikawa Tatsuya <ishikawa11457@gmail.com>
2024-11-12 10:01:12 +0900

    #05

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -8,6 +8,8 @@ public class Goal : MonoBehaviour
 
     public ScoreCount scoreCount;
 
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +29,8 @@ public class Goal : MonoBehaviour
             Destroy(gameObject);
         
             scoreCount.CountUp();
+
+            audioSource.Play();
         }
 
 
